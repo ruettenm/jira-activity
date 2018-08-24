4177c83c1a7aed8992618d9025399e30e3040119
Matthias Ruetten <matthias.ruetten@codecentric.de>
2018-08-24 09:37:53 +0200

    sort output by date

--- src/print.ts
+++ src/print.ts
@@ -1,7 +1,7 @@
 import { GroupedActivities } from './jira'
 
 export const print = (activities: GroupedActivities) => {
-    for (let [ date, entries ] of Object.entries(activities)) {
+    for (let [ date, entries ] of Object.entries(activities).sort()) {
         console.log(`${date}:`)
         for (let [ issueKey, title ] of Object.entries(entries)) {
             console.log(`${issueKey} - ${title}`)
