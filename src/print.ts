82ae00ed86f54e73d8cd3d7fa8887fd78af5785c
Matthias Rütten <matthias.ruetten@codecentric.de>
2019-05-24 12:45:34 +0200

    add a comma as separator to the end of line

--- src/print.ts
+++ src/print.ts
@@ -4,7 +4,7 @@ export const print = (activities: GroupedActivities) => {
     for (let [ date, entries ] of Object.entries(activities).sort()) {
         console.log(`\n${date}:`)
         for (let [ issueKey, title ] of Object.entries(entries)) {
-            console.log(`${issueKey} - ${title}`)
+            console.log(`${issueKey} - ${title},`)
         }
     }
 }
